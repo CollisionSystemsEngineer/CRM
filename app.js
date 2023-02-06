7d0aa34390aab869278c8a19e95a49d14f541f1d
Luis Garcia <a01368192@tec.mx>
2023-02-06 17:51:27 -0600

    Fields changes in patient entity

--- app.js
+++ app.js
@@ -15,60 +15,69 @@ let AtributosDoctores = [{atributo:"Nombre"},
                          {atributo:"Cédula"}, 
                          {atributo:"Especialidad"}, 
                          {atributo:"Cantidad pacientes"}, 
-                         {atributo:"Facturas mensuales"}];
+                         {atributo:"Facturas mensuales"},
+                         {atributo:"Tipo"}];
 
 let Doctores = [{Nombre:"Juanito",
                 Email:"juanito@outlook.com",
                 Cedula:"0894619",
                 Especialidad: "Cardiologo",
                 Pacientes: 20,
-                Facturas: "documentos/facturas/factura1.doc"},
+                Facturas: "documentos/facturas/factura1.doc",
+                Tipo: "Doctor"},
                 {Nombre:"Anita",
                 Email:"anita@outlook.com",
                 Cedula:"0894618",
                 Especialidad: "Pediatra",
                 Pacientes: 30,
-                Facturas: "documentos/facturas/factura2.doc"}];
+                Facturas: "documentos/facturas/factura2.doc",
+                Tipo: "Doctor"}];
 
 let AtributosPacientes = [{atributo:"Nombre"}, 
-                         {atributo:"Email"},
-                         {atributo:"Cédula"}, 
-                         {atributo:"Especialidad"}, 
-                         {atributo:"Cantidad pacientes"}, 
-                         {atributo:"Facturas mensuales"}];
+                         {atributo:"Apellido"},
+                         {atributo:"Email"}, 
+                         {atributo:"Sexo"}, 
+                         {atributo:"Edad"}, 
+                         {atributo:"Foto"},
+                         {atributo:"Tipo"}];
 
 let Pacientes = [{Nombre:"Juanito",
+                Apellido: "Godinez",
                 Email:"juanito@outlook.com",
-                Cedula:"0894619",
-                Especialidad: "Cardiologo",
-                Pacientes: 20,
-                Facturas: "documentos/facturas/factura1.doc"},
-                {Nombre:"Anita",
-                Email:"anita@outlook.com",
-                Cedula:"0894618",
-                Especialidad: "Pediatra",
-                Pacientes: 30,
-                Facturas: "documentos/facturas/factura2.doc"}];
+                Sexo:"Masculino",
+                Edad: 20,
+                Foto: "images/usuario/usuario1.jpg",
+                Tipo: "Paciente"},
+                {Nombre:"Alfonso",
+                Apellido: "Cletus",
+                Email:"cletus@outlook.com",
+                Sexo:"Masculino",
+                Edad: 22,
+                Foto: "images/usuario/usuario1.jpg",
+                Tipo: "Paciente"}];
 
 let AtributosProveedores = [{atributo:"Nombre"}, 
                 {atributo:"Email"},
                 {atributo:"Cédula"}, 
                 {atributo:"Especialidad"}, 
                 {atributo:"Cantidad pacientes"}, 
-                {atributo:"Facturas mensuales"}];
+                {atributo:"Facturas mensuales"},
+                {atributo:"Tipo"}];
 
 let Proveedores = [{Nombre:"Juanito",
        Email:"juanito@outlook.com",
        Cedula:"0894619",
        Especialidad: "Cardiologo",
        Pacientes: 20,
-       Facturas: "documentos/facturas/factura1.doc"},
+       Facturas: "documentos/facturas/factura1.doc",
+        Tipo: "Proveedor"},
        {Nombre:"Anita",
        Email:"anita@outlook.com",
        Cedula:"0894618",
        Especialidad: "Pediatra",
        Pacientes: 30,
-       Facturas: "documentos/facturas/factura2.doc"}];
+       Facturas: "documentos/facturas/factura2.doc",
+        Tipo: "Proveedor"}];
 
 app.get("/admin", function(req,res){
     res.render("admin",{NombreAtributos, AtributosDoctores, Doctores, AtributosPacientes, Pacientes, AtributosProveedores, Proveedores})
